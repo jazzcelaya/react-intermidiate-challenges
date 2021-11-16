a2842eae718720eb7b7e7cfdc5648a284c046219
Jazmin Celaya Sanchez <jazmin.celaya@MacBook-Pro-de-Jazmin.local>
2021-11-16 02:45:02 -0600

    ADD initial setup

---
+++ challenge-2/src/App.js
@@ -0,0 +1,18 @@
+import { StyledBody } from './styled/Body'
+
+function App() {
+  return (
+    <StyledBody>
+      <header>
+      <input type="text" placeholder="Title" />
+        <input type="text" placeholder="Take a note..." />
+      </header>
+      <div>
+        <div>test</div>
+        <div>test</div>
+      </div>
+    </StyledBody>
+  );
+}
+
+export default App;
---
+++ challenge-2/src/styled/Body.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledBody = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: gray;
+  background-color: #f8f8ff;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+`;
